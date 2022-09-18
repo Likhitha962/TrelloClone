0806bd103e44a2e090caf258f9b1b1c9fdf1630c
Likhitha <likkilikki047@gmail.com>
2022-09-18 10:54:43 +0530

    Adding all the base files and designing the splash screen along with the login screen

---
+++ app/src/main/java/com/practice/projectmanage/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.practice.projectmanage
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.WindowManager
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+    var tvAppName : TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        tvAppName = findViewById(R.id.tvAppName)
+//        val typeFace : Typeface = Typeface.createFromAsset(assets,"Nabla-Regular-VariableFont_EDPT,EHLT.ttf"
+        val typeFace : Typeface = Typeface.createFromAsset(assets,"Kanit-Bold.ttf")
+        tvAppName?.typeface = typeFace
+        Handler().postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        },2500)
+    }
+}
